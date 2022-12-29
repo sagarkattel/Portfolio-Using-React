868ebe0a2b363689d679efc96f38edb53ee3ce2a
sagarkattel <71958289+sagarkattel@users.noreply.github.com>
2022-12-29 18:59:30 +0530

    Final

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,36 +1,55 @@
 import React from 'react'
 import './Footer.css'
-import {FaFacebookF} from 'react-icons/fa'
-import {FaInstagram} from 'react-icons/fa'
-import {FaTwitter} from 'react-icons/fa'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
 
 
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Sagar</a>
+      <a href="/#" onClick={() => {
+        window.location.assign('#')}} className='footer__logo'>Sagar</a>
       <ul className='permalinks'>
-      <li><a href="#">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#experience">Experience</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#portfolio">Portfolio</a></li>
-      <li><a href="#testimonials">Testinomials</a></li>
-      <li><a href="#contact">Contact</a></li>
+        <li><button href="#" onClick={() => {
+          window.location.assign('#');
+        }}>Home</button></li>
+        <li><button href="#about" onClick={() => {
+          window.location.assign('#about');
+        }}>About</button></li>
+
+        <li><button href="#experience" onClick={() => {
+          window.location.assign('#experience');
+        }}>Experience</button></li>
+
+
+        <li><button href="#services" onClick={() => {
+          window.location.assign('#services');
+        }}>Works</button></li>
+
+        <li><button href="#testimonials" onClick={() => {
+          window.location.assign('#testimonials');
+        }}>Testimonials</button></li>
+
+        <li><button href="#contact" onClick={() => {
+          window.location.assign('#contact');
+        }}>Contact</button></li>
+
+
       </ul>
       <div className="footer__socials">
         <a href="https://facebook.com"><FaFacebookF /></a>
         <a href="https://instagram.com"><FaInstagram /></a>
-        <a href="https://twitter.com"><FaTwitter/></a>
-        
+        <a href="https://twitter.com"><FaTwitter /></a>
+
       </div>
       <div className="footer__copyright">
         <small>&copy; SAGAR Academy. All rights reserved. </small>
       </div>
-      
+
     </footer>
-    
+
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,42 @@
 import React from 'react'
 import './Nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+import { BiMessageSquareDetail } from 'react-icons/bi'
 import { useState } from 'react'
 
 const Nav = () => {
-  const[activeNav,setActiveNav]=useState('#')
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-    <a href="#" onClick={()=>setActiveNav('#')} className={activeNav==='#'?'active':''}><AiOutlineHome /></a>
+      {/* <a href="#" onClick={()=>setActiveNav('#')} className={activeNav==='#'?'active':''}><AiOutlineHome /></a>
     <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav==='#about'?'active':''}><AiOutlineUser /></a>
     <a href="#experience" onClick={()=>setActiveNav('#experience')} className={activeNav==='#experience'?'active':''}><BiBook /></a>
     <a href="#portfolio" onClick={()=>setActiveNav('#services')} className={activeNav==='#portfolio'?'active':''}><RiServiceLine /></a>
-    <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}><BiMessageSquareDetail /></a>
+    <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}><BiMessageSquareDetail /></a> */}
+
+      <button onClick={() => {
+        window.location.assign('#');
+        setActiveNav('#');
+      }} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></button>
+      <button onClick={() => {
+        window.location.assign('#about');
+        setActiveNav('#about');
+      }} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></button>
+      <button onClick={() => {
+        window.location.assign('#experience');
+        setActiveNav('#experience');
+      }} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></button>
+      <button onClick={() => {
+        window.location.assign('#portfolio');
+        setActiveNav('#portfolio');
+      }} className={activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine /></button>
+      <button onClick={() => {
+        window.location.assign('#contact');
+        setActiveNav('#contact');
+      }} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></button>
     </nav>
   )
 }
